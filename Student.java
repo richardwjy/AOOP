10cbc97ccca35d6e9106f36f0fd50aa5653b7b65
Richard <49726866+richardwjy@users.noreply.github.com>
2019-10-17 12:20:58 +0700

    Add files via upload
    first commit

---
+++ Student.java
@@ -0,0 +1,19 @@
+package pertemuan4;
+
+public class Student extends People{
+
+	private String nim;
+	private Integer kodeKelas;
+	
+	public Student(){
+		
+	}	
+	public Student(String nim, Integer kodeKelas) {
+		super();
+		this.nim = nim;
+		this.kodeKelas = kodeKelas;
+	}
+	
+	
+	
+}
